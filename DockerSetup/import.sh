20b65d178f616e62d8d9e9ab0db33f7b687703fb
andy <andy@webscale.com.au>
2020-11-06 09:47:33 +0000

    bug fixing

--- DockerSetup/import.sh
+++ DockerSetup/import.sh
@@ -14,29 +14,33 @@ echo "Restoring Payroll_Common"
 /opt/mssql-tools/bin/sqlcmd -U sa -P SaPassword1 -Q "
 	DECLARE @DatabaseName VARCHAR(128) 
 	DECLARE @DatabaseFilename VARCHAR(128) 
-	SET @DatabaseName = 'Payroll_Common' 
-	SET @DatabaseFilename = '/backups/Payroll_Common.bak' 
 	DECLARE @UseStatement VARCHAR(128) 
-	SET @UseStatement = (SELECT 'USE [' + @DatabaseName + ']') 
 	DECLARE @DataName VARCHAR(128) 
 	DECLARE @LogsName VARCHAR(128) 
 	DECLARE @NewDataLocation VARCHAR(128) 
 	DECLARE @NewLogsLocation VARCHAR(128) 
 	DECLARE @filelist TABLE (LogicalName varchar(128),[PhysicalName] varchar(128),[Type] varchar,[FileGroupName] varchar(128),[Size] varchar(128),[MaxSize] varchar(128),[FileId]varchar(128),[CreateLSN]varchar(128),[DropLSN]varchar(128),[UniqueId]varchar(128),[ReadOnlyLSN]varchar(128),[ReadWriteLSN]varchar(128),[BackupSizeInBytes]varchar(128),[SourceBlockSize]varchar(128),[FileGroupId]varchar(128),[LogGroupGUID]varchar(128),[DifferentialBaseLSN]varchar(128),[DifferentialBaseGUID]varchar(128),[IsReadOnly]varchar(128),[IsPresent]varchar(128),[TDEThumbprint]varchar(128),[SnapshotUrl]varchar(2000)) 
+
+	SET @DatabaseName = 'Payroll_Common' 
+	SET @DatabaseFilename = '/backups/Payroll_Common.bak' 	
+	SET @UseStatement = (SELECT 'USE [' + @DatabaseName + ']') 
+	
 	INSERT INTO @filelist EXEC('RESTORE FILELISTONLY FROM DISK=''' + @DatabaseFilename + ''' ') 
 	SET @NewDataLocation = (SELECT '/var/opt/mssql/data/' + @DatabaseName + '.mdf'); 
 	SET @NewLogsLocation = (SELECT '/var/opt/mssql/data/' + @DatabaseName + '_log.ldf'); 
 	SET @DataName =(select LogicalName from @filelist where [Type] ='D') 
 	SET @LogsName = (select LogicalName from @filelist where [Type] ='L') 
+
 	RESTORE DATABASE @DatabaseName FROM DISK=@DatabaseFilename WITH MOVE @DataName TO @NewDataLocation, MOVE @LogsName TO @NewLogsLocation 
-    EXEC sp_sqlexec @UseStatement 
-	CREATE USER payroll FOR LOGIN payroll 
+
+    EXEC sp_sqlexec @UseStatement
+
+	CREATE USER payroll FOR LOGIN payroll
+
 	ALTER DATABASE Payroll_Common SET RECOVERY SIMPLE; 
+
 	DBCC SHRINKFILE (@LogsName, 1);
 "
-
-echo "Setting recovery to SIMPLE and clearing Transaction Logs"
-/opt/mssql-tools/bin/sqlcmd -U sa -P SaPassword1 -Q "ALTER DATABASE Payroll_Common SET RECOVERY SIMPLE"
 
 echo "Setting up Whitelabel Alias for localhost"
 /opt/mssql-tools/bin/sqlcmd -U sa -P SaPassword1 -Q "UPDATE Payroll_Common.dbo.WhiteLabelAlias SET WhiteLabelId = (SELECT Id FROM Payroll_Common.dbo.WhiteLabelShard WHERE HostName='keypay.yourpayroll.co.uk') WHERE HostName='localhost'"
@@ -45,30 +49,31 @@ echo "Restoring Payroll_Shard4"
 /opt/mssql-tools/bin/sqlcmd -U sa -P SaPassword1 -Q "
 	DECLARE @DatabaseName VARCHAR(128)	
 	DECLARE @DatabaseFilename VARCHAR(128)
-	SET @DatabaseName = 'Payroll_Shard4'	
-	SET @DatabaseFilename = '/backups/Payroll_Shard4.bak'
-	
 	DECLARE @UseStatement VARCHAR(128)
-	SET @UseStatement = (SELECT 'USE [' + @DatabaseName + ']')
-	
 	DECLARE @DataName VARCHAR(128)
 	DECLARE @LogsName VARCHAR(128)
 	DECLARE @NewDataLocation VARCHAR(128)
-	DECLARE @NewLogsLocation VARCHAR(128)
-		
+	DECLARE @NewLogsLocation VARCHAR(128)		
 	DECLARE @filelist TABLE (LogicalName varchar(128),[PhysicalName] varchar(128),[Type] varchar,[FileGroupName] varchar(128),[Size] varchar(128),[MaxSize] varchar(128),[FileId]varchar(128),[CreateLSN]varchar(128),[DropLSN]varchar(128),[UniqueId]varchar(128),[ReadOnlyLSN]varchar(128),[ReadWriteLSN]varchar(128),[BackupSizeInBytes]varchar(128),[SourceBlockSize]varchar(128),[FileGroupId]varchar(128),[LogGroupGUID]varchar(128),[DifferentialBaseLSN]varchar(128),[DifferentialBaseGUID]varchar(128),[IsReadOnly]varchar(128),[IsPresent]varchar(128),[TDEThumbprint]varchar(128),[SnapshotUrl]varchar(2000))
+	
+	SET @DatabaseName = 'Payroll_Shard4'	
+	SET @DatabaseFilename = '/backups/Payroll_Shard4.bak'	
+	SET @UseStatement = (SELECT 'USE [' + @DatabaseName + ']')
+		
 	INSERT INTO @filelist EXEC('RESTORE FILELISTONLY FROM DISK=''' + @DatabaseFilename + ''' ')
-
 	SET @NewDataLocation = (SELECT '/var/opt/mssql/data/' + @DatabaseName + '.mdf');
 	SET @NewLogsLocation = (SELECT '/var/opt/mssql/data/' + @DatabaseName + '_log.ldf');
 	SET @DataName =(select LogicalName from @filelist where [Type] ='D')
 	SET @LogsName = (select LogicalName from @filelist where [Type] ='L')
+	
 	RESTORE DATABASE @DatabaseName FROM DISK=@DatabaseFilename WITH MOVE @DataName TO @NewDataLocation, MOVE @LogsName TO @NewLogsLocation
     	
 	EXEC sp_sqlexec @UseStatement	
+
 	CREATE USER payroll FOR LOGIN payroll
 	
 	ALTER DATABASE Payroll_Shard4 SET RECOVERY SIMPLE; 
+
 	DBCC SHRINKFILE (@LogsName, 1);
 "
 
@@ -76,60 +81,30 @@ echo "Restoring Payroll_Shard5"
 /opt/mssql-tools/bin/sqlcmd -U sa -P SaPassword1 -Q "
 	DECLARE @DatabaseName VARCHAR(128)	
 	DECLARE @DatabaseFilename VARCHAR(128)
-	SET @DatabaseName = 'Payroll_Shard5'	
-	SET @DatabaseFilename = '/backups/Payroll_Shard5.bak'
-	
 	DECLARE @UseStatement VARCHAR(128)
-	SET @UseStatement = (SELECT 'USE [' + @DatabaseName + ']')
-	
 	DECLARE @DataName VARCHAR(128)
 	DECLARE @LogsName VARCHAR(128)
 	DECLARE @NewDataLocation VARCHAR(128)
-	DECLARE @NewLogsLocation VARCHAR(128)
-		
+	DECLARE @NewLogsLocation VARCHAR(128)		
 	DECLARE @filelist TABLE (LogicalName varchar(128),[PhysicalName] varchar(128),[Type] varchar,[FileGroupName] varchar(128),[Size] varchar(128),[MaxSize] varchar(128),[FileId]varchar(128),[CreateLSN]varchar(128),[DropLSN]varchar(128),[UniqueId]varchar(128),[ReadOnlyLSN]varchar(128),[ReadWriteLSN]varchar(128),[BackupSizeInBytes]varchar(128),[SourceBlockSize]varchar(128),[FileGroupId]varchar(128),[LogGroupGUID]varchar(128),[DifferentialBaseLSN]varchar(128),[DifferentialBaseGUID]varchar(128),[IsReadOnly]varchar(128),[IsPresent]varchar(128),[TDEThumbprint]varchar(128),[SnapshotUrl]varchar(2000))
+	
+	SET @DatabaseName = 'Payroll_Shard5'	
+	SET @DatabaseFilename = '/backups/Payroll_Shard5.bak'	
+	SET @UseStatement = (SELECT 'USE [' + @DatabaseName + ']')
+		
 	INSERT INTO @filelist EXEC('RESTORE FILELISTONLY FROM DISK=''' + @DatabaseFilename + ''' ')
-
 	SET @NewDataLocation = (SELECT '/var/opt/mssql/data/' + @DatabaseName + '.mdf');
 	SET @NewLogsLocation = (SELECT '/var/opt/mssql/data/' + @DatabaseName + '_log.ldf');
 	SET @DataName =(select LogicalName from @filelist where [Type] ='D')
 	SET @LogsName = (select LogicalName from @filelist where [Type] ='L')
+	
 	RESTORE DATABASE @DatabaseName FROM DISK=@DatabaseFilename WITH MOVE @DataName TO @NewDataLocation, MOVE @LogsName TO @NewLogsLocation
     	
 	EXEC sp_sqlexec @UseStatement	
+
 	CREATE USER payroll FOR LOGIN payroll
 	
 	ALTER DATABASE Payroll_Shard5 SET RECOVERY SIMPLE; 
-	DBCC SHRINKFILE (@LogsName, 1);
-"
-
-echo "Restoring Payroll_Shard2"
-/opt/mssql-tools/bin/sqlcmd -U sa -P SaPassword1 -Q "
-	DECLARE @DatabaseName VARCHAR(128)	
-	DECLARE @DatabaseFilename VARCHAR(128)
-	SET @DatabaseName = 'Payroll_Shard2'	
-	SET @DatabaseFilename = '/backups/Payroll_Shard2.bak'
 	
-	DECLARE @UseStatement VARCHAR(128)
-	SET @UseStatement = (SELECT 'USE [' + @DatabaseName + ']')
-	
-	DECLARE @DataName VARCHAR(128)
-	DECLARE @LogsName VARCHAR(128)
-	DECLARE @NewDataLocation VARCHAR(128)
-	DECLARE @NewLogsLocation VARCHAR(128)
-		
-	DECLARE @filelist TABLE (LogicalName varchar(128),[PhysicalName] varchar(128),[Type] varchar,[FileGroupName] varchar(128),[Size] varchar(128),[MaxSize] varchar(128),[FileId]varchar(128),[CreateLSN]varchar(128),[DropLSN]varchar(128),[UniqueId]varchar(128),[ReadOnlyLSN]varchar(128),[ReadWriteLSN]varchar(128),[BackupSizeInBytes]varchar(128),[SourceBlockSize]varchar(128),[FileGroupId]varchar(128),[LogGroupGUID]varchar(128),[DifferentialBaseLSN]varchar(128),[DifferentialBaseGUID]varchar(128),[IsReadOnly]varchar(128),[IsPresent]varchar(128),[TDEThumbprint]varchar(128),[SnapshotUrl]varchar(2000))
-	INSERT INTO @filelist EXEC('RESTORE FILELISTONLY FROM DISK=''' + @DatabaseFilename + ''' ')
-
-	SET @NewDataLocation = (SELECT '/var/opt/mssql/data/' + @DatabaseName + '.mdf');
-	SET @NewLogsLocation = (SELECT '/var/opt/mssql/data/' + @DatabaseName + '_log.ldf');
-	SET @DataName =(select LogicalName from @filelist where [Type] ='D')
-	SET @LogsName = (select LogicalName from @filelist where [Type] ='L')
-	RESTORE DATABASE @DatabaseName FROM DISK=@DatabaseFilename WITH MOVE @DataName TO @NewDataLocation, MOVE @LogsName TO @NewLogsLocation
-    	
-	EXEC sp_sqlexec @UseStatement	
-	CREATE USER payroll FOR LOGIN payroll
-	
-	ALTER DATABASE Payroll_Shard2 SET RECOVERY SIMPLE; 
 	DBCC SHRINKFILE (@LogsName, 1);
 "
