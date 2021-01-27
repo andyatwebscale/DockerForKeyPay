44acb044b3d74359e5ccffc701d39888ec630a47
Anders Branderud <anders.branderud@gmail.com>
2021-01-27 11:09:02 +0000

    shard 2,4,5 line endings,etc

--- RestoreScripts/Clean and Build SQL for Shard 2, 4 and 5 in backup folder.ps1
+++ RestoreScripts/Clean and Build SQL for Shard 2, 4 and 5 in backup folder.ps1
@@ -1,6 +1,6 @@
-docker stop sql2019
+docker stop sql2019-shard245
 
-docker rm sql2019
+docker rm sql2019-shard245
 
 docker rmi database
 
@@ -8,4 +8,4 @@ cd C:\Projects\DockerForKeyPay\DockerSetupShard245
 
 docker build -t database .
 
-docker run --name "sql2019"  -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=SaPassword1" -p 1433:1433 -v ~/C/DbBackupFiles:/backups database
+docker run --name "sql2019-shard245"  -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=SaPassword1" -p 1433:1433 -v ~/C/DbBackupFiles:/backups database
